97324c93e0c2ad635e66f3727dfe7f21bbde61b5
Leonardo Santos <137180866+LeonardoSantosmog@users.noreply.github.com>
2025-08-13 14:39:24 -0300

    Add files via upload

---
+++ temperatura.py
@@ -0,0 +1,11 @@
+Temperatura = float(input("Digite a temperatura em Celsius: "))
+if Temperatura < 11:
+    print("A temperatura esta gelada🥶🧊")
+elif 11 <= Temperatura < 20:
+    print("A temperatura esta normal😁😄")
+elif 20 <= Temperatura > 30:
+    print("A temperatura esta quente🔥🥵")
+
+else:
+    print("Obrigado por usar o programa! ate mais!👋")
+    
